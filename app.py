f2173ad46a897a89d1dd0885f87ce097ff99c67e
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-10-25 23:04:15 +0200

    Added comments

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from website import create_app
 
+# Run only when started as standalone
+# This prevents running the code when loading
 if __name__ == '__main__':
+	# Get an Flask app instance
 	app = create_app()
+	# Start the app with debug enabled
 	app.run( debug = True, port = 8000 )
--- website/__init__.py
+++ website/__init__.py
@@ -7,20 +7,33 @@ db = SQLAlchemy()
 DB_NAME = 'database.db'
 
 def create_app():
+	"""
+	Set up Flask app instance
+	Set up sqlite database
+	Setup Flask blueprints
+	"""
+
 	from .views import views
 	from .auth import auth
 	from .models import User, Movie
 
+	# Set up Flask app instance
 	app = Flask( __name__ )
+	# Create a secret key that prevents coockie manipulation
 	app.config[ 'SECRET_KEY' ] = 'testch03taau3df8rgwhh6jghtest'
+	# Set up a database URI with protocol and filename
 	app.config[ 'SQLALCHEMY_DATABASE_URI' ] = f'sqlite:///{ DB_NAME }'
 	db.init_app( app )
 
+	# Web functionality is in separate modules
+	# Separate pages for logged in users from pages for not logged in users using Flask blueprints
 	app.register_blueprint( views, url_prefix = '/' )
 	app.register_blueprint( auth, url_prefix = '/' )
 
+	# Make sure the database exists
 	create_database( app )
 
+	# Set up login logic
 	login_manager = LoginManager()
 	login_manager.login_view = 'auth.login_get'
 	login_manager.init_app( app )
@@ -31,7 +44,16 @@ def create_app():
 
 	return app
 
+
+
 def create_database( app ) :
+	"""
+	Checks if the file containing the database is present
+	Creates the database file if not
+	:param app: Flask app
+	:return: Nothing
+	"""
+
 	if not path.exists( 'website/' + DB_NAME ) :
 		db.create_all( app = app )
 		print( 'Created database!' )
