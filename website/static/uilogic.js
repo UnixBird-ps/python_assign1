359e5d5484d2e99413d6fbbd289751f5d684fa0a
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-10-30 00:20:49 +0200

    Added prevention of unnecessery update of poster image

--- website/static/uilogic.js
+++ website/static/uilogic.js
@@ -24,7 +24,7 @@ function doneMovie( pMovieId )
 {
 	var lList = document.getElementById( 'movies' );
 	var lListItem = lList.querySelector( 'li#li_' + pMovieId );
-	var lCheckBox = lListItem.querySelector ( 'input[id^="checkbox_"' );
+	var lCheckBox = lListItem.querySelector( 'input[id^="checkbox_"' );
 	fetch( '/done-movie',
 		{
 			method : 'POST',
@@ -37,39 +37,54 @@ function doneMovie( pMovieId )
 
 function updateTitle( pMovieId, pInputBox )
 {
-	var lTitleStr = pInputBox.value;
-	fetch( '/update-movie-title',
-		{
-			method : 'POST',
-			body : JSON.stringify( { id : pMovieId, title : lTitleStr } )
-		}
-	).then( ( _res ) => {} );
+	if ( pInputBox.value != pInputBox.dataset.lastvalue )
+	{
+		pInputBox.dataset.lastvalue = pInputBox.value;
+		console.log( 'Title changed' );
+
+		fetch( '/update-movie-title',
+			{
+				method : 'POST',
+				body : JSON.stringify( { id : pMovieId, title : pInputBox.value } )
+			}
+		).then( ( _res ) => {} );
+	}
 }
 
 
 
 function updateGenre( pMovieId, pInputBox )
 {
-	var lGenreStr = pInputBox.value;
-	fetch( '/update-movie-genre',
-		{
-			method : 'POST',
-			body : JSON.stringify( { id : pMovieId, genre : lGenreStr } )
-		}
-	).then( ( _res ) => {} );
+	if ( pInputBox.value != pInputBox.dataset.lastvalue )
+	{
+		pInputBox.dataset.lastvalue = pInputBox.value;
+		console.log( 'Genre changed' );
+
+		fetch( '/update-movie-genre',
+			{
+				method : 'POST',
+				body : JSON.stringify( { id : pMovieId, genre : pInputBox.value } )
+			}
+		).then( ( _res ) => {} );
+	}
 }
 
 
 
 function updateLength( pMovieId, pInputBox )
 {
-	var lLength = pInputBox.value;
-	fetch( '/update-movie-length',
-		{
-			method : 'POST',
-			body : JSON.stringify( { id : pMovieId, length : lLength } )
-		}
-	).then( ( _res ) => {} );
+	if ( pInputBox.value != pInputBox.dataset.lastvalue )
+	{
+		pInputBox.dataset.lastvalue = pInputBox.value;
+		console.log( 'Length changed' );
+
+		fetch( '/update-movie-length',
+			{
+				method : 'POST',
+				body : JSON.stringify( { id : pMovieId, length : pInputBox.value } )
+			}
+		).then( ( _res ) => {} );
+	}
 }
 
 
@@ -89,28 +104,54 @@ function renumTableRows()
 
 
 
+/**
 
+*/
 function updateModalPoster( pMovieId, pImg )
 {
-	document.getElementById( 'big_poster' ).src = pImg.src;
-	document.getElementById( 'poster_src' ).value = pImg.src;
-	document.getElementById( 'submit_poster_url' ).outerHTML = `<button id="submit_poster_url" class="btn btn-primary" data-bs-dismiss="modal" aria-label="Update poster" value="Update" onclick="updatePoster( ${pMovieId} )">Update</button>`;
+	document.getElementById( 'big_poster_img' ).src = pImg.src;
+	var lInputBox = document.getElementById( 'poster_src' );
+	lInputBox.value = pImg.src;
+	console.log( lInputBox.dataset.lastvalue );
+	lInputBox.dataset.lastvalue = lInputBox.value;
+	console.log( lInputBox.dataset.lastvalue );
+	document.getElementById( 'submit_poster_url' ).outerHTML = `<button id="submit_poster_url" class="btn btn-primary" data-bs-dismiss="modal" aria-label="Update poster" value="Update" onclick="updateMoviePoster( ${pMovieId} )">Update</button>`;
 }
 
 
 
-function updatePoster( pMovieId )
+/**
+
+*/
+function updateBigPosterUrl( pInputBox )
 {
+	if ( pInputBox.value != pInputBox.dataset.lastvalue )
+	{
+		pInputBox.dataset.lastvalue = pInputBox.value;
+		console.log( 'Poster URL has changed' );
+
+		document.getElementById( 'big_poster_img' ).src = pInputBox.value
+	}
+}
+
+
+
+/**
+
+*/
+function updateMoviePoster( pMovieId, pUrl )
+{
+	var lInputBox = document.getElementById( 'poster_src' );
 	fetch( '/update-poster',
 		{
 			method : 'POST',
-			body : JSON.stringify( { id : pMovieId, poster_url : document.getElementById( 'poster_src' ).value } )
+			body : JSON.stringify( { id : pMovieId, poster_url : lInputBox.value } )
 		}
 	).then(
 		( _res ) =>
 		{
 			var lPosterIcon = document.getElementById( 'poster_icon_' + pMovieId );
-			lPosterIcon.src = document.getElementById( 'poster_src' ).value;
+			lPosterIcon.src = lInputBox.value;
 		}
 	);
 }
