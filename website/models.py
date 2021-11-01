c6761ff0f3b2396e39ee1bed2f46f210fa0104cb
ps74lab <88337865+ps74lab@users.noreply.github.com>
2021-11-01 19:40:45 +0100

    Removed outcommented code

--- website/models.py
+++ website/models.py
@@ -1,6 +1,5 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
 from sqlalchemy.ext.orderinglist import ordering_list
 
 
@@ -15,9 +14,7 @@ class Movie( db.Model ) :
 	genre = db.Column( db.String( 50 ) )
 	length = db.Column( db.Integer ) # minutes
 	done = db.Column( db.Boolean )
-	#date = db.Column( db.DateTime( timezone = True), default = func.now() )
 	position = db.Column( db.Integer )
-	#user_id = db.Column( db.Integer, db.ForeignKey( 'user.id' ) )
 	user_id = db.Column( db.Integer, db.ForeignKey( 'user.id' ) )
 
 
@@ -25,11 +22,11 @@ class Movie( db.Model ) :
 class User( db.Model, UserMixin ) :
 	"""
 	Holds user account information and a reference to movies associated with that account
+	Does reordering of movies automatic
 	"""
 	#__tablename__ = 'user'
 	id = db.Column( db.Integer, primary_key = True )
 	email = db.Column( db.String( 150 ), unique = True )
 	password = db.Column( db.String( 150 ) )
 	first_name = db.Column( db.String( 150 ) )
-	#movies = db.relationship( 'movie', order_by = 'movie.position', collection_class = ordering_list( 'movie.position' ) )
-	movies = db.relationship( 'Movie', order_by = 'Movie.position', collection_class = ordering_list( 'position', count_from=0 ) )
+	movies = db.relationship( 'Movie', order_by = 'Movie.position', collection_class = ordering_list( 'position' ) )
